3a19701c21bbf3815d9d9609d638943005bd1970
tobias-andrade <64922512+tobias-andrade@users.noreply.github.com>
2022-04-22 20:58:57 -0300

    Add files via upload

--- receptor/src/Vistas/PanelReceptor.java
+++ receptor/src/Vistas/PanelReceptor.java
@@ -1,5 +1,6 @@
 package Vistas;
 
+import java.net.InetAddress;
 import java.applet.AudioClip;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
@@ -23,6 +24,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.awt.event.ActionEvent;
 import java.awt.SystemColor;
 import javax.swing.border.SoftBevelBorder;
@@ -36,27 +38,33 @@ public class PanelReceptor extends JFrame implements IVista {
 
 	private static PanelReceptor instance;
 	private JPanel contentPane;
+	private JPanel panel_10;
+	private JPanel panel_11;
+	private JPanel panel_12;
+	private JLabel lblNewLabel_3;
 	private JPanel panel_2;
+	private JRadioButton rdbtnAsistenciaMedica;
+	private JRadioButton rdbtnSeguridad;
+	private JRadioButton rdbtnIncendio;
 	private JPanel panel_5;
+	private JLabel lblTipoDeSolicitud_1;
 	private JPanel panel;
-	private JLabel lblTipoDeSolicitud;
-	private JPanel panel_1;
 	private JTextField textField_tipo;
+	private JPanel panel_1;
+	private JLabel lblNewLabel_2;
 	private JPanel panel_3;
-	private JLabel lblNewLabel;
-	private JPanel panel_4;
 	private JTextField textField_hora;
+	private JPanel panel_4;
+	private JLabel lblNewLabel;
 	private JPanel panel_6;
-	private JLabel lblNewLabel_1;
+	private JTextField textField_lugar;
 	private JPanel panel_7;
 	private JPanel panel_8;
-	private JPanel panel_9;
 	private JButton btn_ConfirmarRecepcion;
-	private JRadioButton rdbtnAsistenciaMedica;
-	private JRadioButton rdbtnNewRadioButton_1;
-	private JRadioButton rdbtnNewRadioButton_2;
-	private JLabel lblNewLabel_2;
-	private JTextField textField_lugar;
+	private JLabel lblNewLabel_1;
+	private JLabel lblNewLabel_4;
+	private JTextField textFieldPuerto;
+	private JTextField textFieldIP;
 
 	/**
 	 * Launch the application.
@@ -119,105 +127,149 @@ public class PanelReceptor extends JFrame implements IVista {
 		this.contentPane = new JPanel();
 		this.contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(this.contentPane);
-		this.contentPane.setLayout(new GridLayout(5, 2, 0, 0));
-
+		this.contentPane.setLayout(new GridLayout(6, 2, 0, 0));
+		
+		this.panel_10 = new JPanel();
+		this.contentPane.add(this.panel_10);
+		this.panel_10.setLayout(null);
+		
+		this.lblNewLabel_1 = new JLabel("IP:");
+		this.lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		this.lblNewLabel_1.setBounds(84, 20, 57, 26);
+		this.panel_10.add(this.lblNewLabel_1);
+		
+		this.lblNewLabel_4 = new JLabel("Puerto:");
+		this.lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		this.lblNewLabel_4.setBounds(71, 50, 103, 28);
+		this.panel_10.add(this.lblNewLabel_4);
+		
+		this.panel_11 = new JPanel();
+		this.contentPane.add(this.panel_11);
+		this.panel_11.setLayout(null);
+		
+		this.textFieldPuerto = new JTextField();
+		this.textFieldPuerto.setText("1233");
+		this.textFieldPuerto.setEditable(false);
+		this.textFieldPuerto.setBounds(35, 55, 163, 20);
+		this.panel_11.add(this.textFieldPuerto);
+		this.textFieldPuerto.setColumns(10);
+		
+		this.textFieldIP = new JTextField();
+		this.textFieldIP.setEditable(false);
+		this.textFieldIP.setBounds(35, 20, 163, 20);
+		this.panel_11.add(this.textFieldIP);
+		this.textFieldIP.setColumns(10);
+		
+		this.panel_12 = new JPanel();
+		this.contentPane.add(this.panel_12);
+		this.panel_12.setLayout(null);
+		
+		this.lblNewLabel_3 = new JLabel("Solicitudes permitidas:");
+		this.lblNewLabel_3.setBounds(20, 22, 207, 46);
+		this.lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		this.panel_12.add(this.lblNewLabel_3);
+		
 		this.panel_2 = new JPanel();
 		this.contentPane.add(this.panel_2);
-		this.panel_2.setLayout(new BorderLayout(0, 0));
-
-		this.lblNewLabel_2 = new JLabel("Solicitudes permitidas:");
-		this.lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		this.panel_2.add(this.lblNewLabel_2);
-
-		this.panel_5 = new JPanel();
-		this.contentPane.add(this.panel_5);
-		this.panel_5.setLayout(new GridLayout(3, 1, 0, 0));
-
+		this.panel_2.setLayout(null);
+		
 		this.rdbtnAsistenciaMedica = new JRadioButton("Asistencia Medica");
-		this.panel_5.add(this.rdbtnAsistenciaMedica);
-
-		this.rdbtnNewRadioButton_1 = new JRadioButton("Personal de seguridad");
-		this.panel_5.add(this.rdbtnNewRadioButton_1);
-
-		this.rdbtnNewRadioButton_2 = new JRadioButton("Foco de incendio");
-		this.panel_5.add(this.rdbtnNewRadioButton_2);
-
+		this.rdbtnAsistenciaMedica.setBounds(34, 0, 237, 30);
+		this.panel_2.add(this.rdbtnAsistenciaMedica);
+		
+		this.rdbtnSeguridad = new JRadioButton("Personal de seguridad");
+		this.rdbtnSeguridad.setBounds(34, 29, 237, 30);
+		this.panel_2.add(this.rdbtnSeguridad);
+		
+		this.rdbtnIncendio = new JRadioButton("Foco de incendio");
+		this.rdbtnIncendio.setBounds(34, 60, 237, 30);
+		this.panel_2.add(this.rdbtnIncendio);
+		
+		this.panel_5 = new JPanel();
+		this.panel_5.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		this.contentPane.add(this.panel_5);
+		this.panel_5.setLayout(null);
+		
+		this.lblTipoDeSolicitud_1 = new JLabel("Tipo de Solicitud");
+		this.lblTipoDeSolicitud_1.setBounds(10, 40, 231, 25);
+		this.lblTipoDeSolicitud_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		this.panel_5.add(this.lblTipoDeSolicitud_1);
+		
 		this.panel = new JPanel();
 		this.panel.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		this.contentPane.add(this.panel);
-		this.panel.setLayout(new BorderLayout(0, 0));
-
-		this.lblTipoDeSolicitud = new JLabel("Tipo de Solicitud");
-		this.lblTipoDeSolicitud.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		this.panel.add(this.lblTipoDeSolicitud);
-
-		this.panel_1 = new JPanel();
-		this.panel_1.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		this.contentPane.add(this.panel_1);
-		this.panel_1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 40));
-
+		this.panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 40));
+		
 		this.textField_tipo = new JTextField();
 		this.textField_tipo.setEditable(false);
 		this.textField_tipo.setColumns(20);
 		this.textField_tipo.setBackground(Color.WHITE);
-		this.panel_1.add(this.textField_tipo);
-
+		this.panel.add(this.textField_tipo);
+		
+		this.panel_1 = new JPanel();
+		this.panel_1.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		this.contentPane.add(this.panel_1);
+		this.panel_1.setLayout(null);
+		
+		this.lblNewLabel_2 = new JLabel("Hora");
+		this.lblNewLabel_2.setBounds(10, 40, 231, 25);
+		this.lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		this.panel_1.add(this.lblNewLabel_2);
+		
 		this.panel_3 = new JPanel();
 		this.panel_3.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		this.contentPane.add(this.panel_3);
-		this.panel_3.setLayout(new BorderLayout(0, 0));
-
-		this.lblNewLabel = new JLabel("Hora");
-		this.lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		this.panel_3.add(this.lblNewLabel);
-
-		this.panel_4 = new JPanel();
-		this.panel_4.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		this.contentPane.add(this.panel_4);
-		this.panel_4.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 40));
-
+		this.panel_3.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 40));
+		
 		this.textField_hora = new JTextField();
 		this.textField_hora.setEditable(false);
 		this.textField_hora.setColumns(20);
 		this.textField_hora.setBackground(Color.WHITE);
-		this.panel_4.add(this.textField_hora);
-
+		this.panel_3.add(this.textField_hora);
+		
+		this.panel_4 = new JPanel();
+		this.panel_4.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		this.contentPane.add(this.panel_4);
+		this.panel_4.setLayout(null);
+		
+		this.lblNewLabel = new JLabel("Lugar");
+		this.lblNewLabel.setBounds(10, 39, 231, 25);
+		this.lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		this.panel_4.add(this.lblNewLabel);
+		
 		this.panel_6 = new JPanel();
 		this.panel_6.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		this.contentPane.add(this.panel_6);
-		this.panel_6.setLayout(new BorderLayout(0, 0));
-
-		this.lblNewLabel_1 = new JLabel("Lugar");
-		this.lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		this.panel_6.add(this.lblNewLabel_1);
-
-		this.panel_7 = new JPanel();
-		this.panel_7.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		this.contentPane.add(this.panel_7);
-		this.panel_7.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 40));
+		this.panel_6.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 40));
 		
 		this.textField_lugar = new JTextField();
-		this.textField_lugar.setBackground(Color.WHITE);
 		this.textField_lugar.setEditable(false);
-		this.panel_7.add(this.textField_lugar);
 		this.textField_lugar.setColumns(20);
-
+		this.textField_lugar.setBackground(Color.WHITE);
+		this.panel_6.add(this.textField_lugar);
+		
+		this.panel_7 = new JPanel();
+		this.contentPane.add(this.panel_7);
+		
 		this.panel_8 = new JPanel();
 		this.contentPane.add(this.panel_8);
-
-		this.panel_9 = new JPanel();
-		this.contentPane.add(this.panel_9);
-		this.panel_9.setLayout(new FlowLayout(FlowLayout.CENTER, 50, 40));
-
+		this.panel_8.setLayout(null);
+		
 		this.btn_ConfirmarRecepcion = new JButton("Confirmar Recepcion");
-		this.btn_ConfirmarRecepcion.setActionCommand("ConfirmarRecepcion");
+		this.btn_ConfirmarRecepcion.setBounds(29, 31, 179, 29);
 		this.btn_ConfirmarRecepcion.setToolTipText("");
 		this.btn_ConfirmarRecepcion.setForeground(Color.WHITE);
 		this.btn_ConfirmarRecepcion.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		this.btn_ConfirmarRecepcion.setBackground(Color.BLACK);
-		this.panel_9.add(this.btn_ConfirmarRecepcion);
+		this.btn_ConfirmarRecepcion.setActionCommand("ConfirmarRecepcion");
+		this.panel_8.add(this.btn_ConfirmarRecepcion);
 		this.setVisible(true);
-		
+		try {
+			this.textFieldIP.setText(InetAddress.getLocalHost().getHostAddress());
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public void getFieldLugar(String a) {
